4b77089cede395f97a47419a9d1be9b63cae639e
Shilpa <138445884+Shilpasurendran07@users.noreply.github.com>
2024-12-17 19:06:33 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //all code treated as newer version code
+//alert(3+3) //error- we are using node js not the browser
+//code readablility should be kept in mind
+
+let name="Shilpa";
+let age=28
+let isLoggedIn=true;
+
+//primitive data types
+//number=> ranges 2 pw 53
+//string => inside double quotes or single quotes
+//boolean => true/ false
+//bigInt
+//null => standalone value, representation of empty
+//undefined => value is not defined
+//symbol => majorly used in react, to identify component uniqueness
+
+
+//non primitive
+//object
+
+console.log(typeof "shilpa")
+//type of null => object
+//type of undefined => undefines
+console.log(typeof null);
+console.log(typeof undefined);
+
