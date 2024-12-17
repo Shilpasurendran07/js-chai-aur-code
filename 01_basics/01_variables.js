4d62de9a70d6e1362db65ce77095e563bc44c884
Shilpa <138445884+Shilpasurendran07@users.noreply.github.com>
2024-12-17 15:13:08 +0000

    variables and constants

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234556;
+let accountEmail= "shilpa.cs96@gmail.com";
+var accountPassword= '12345'; //var is valid but try not to use it becoz of block scope and functional scope
+accCity="Kochi"; //not recommended method
+// accountId=1010101;
+let accountBranch; //only defined and no value assigned the value becomes undefined
+console.log(accountId);
+accountEmail="scs@gmail.com"
+accountPassword='10101'
+accCity="bengaluru"
+console.table([accountEmail,accountPassword,accCity,accountBranch])
