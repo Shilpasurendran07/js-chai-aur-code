0d77a3520792065a96f7cf08f51bcacef305ea90
Shilpa <138445884+Shilpasurendran07@users.noreply.github.com>
2024-12-18 10:21:10 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+// let score=33
+// let score="33"
+// let score = "33ebc"
+// let score=null
+let score=undefined
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
+//33=>NUMBER
+//33EBD=>Nan[notanumber]
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=> true
+//""=>false
+//"hitesh"=>true
+
+let someNumber=35
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
